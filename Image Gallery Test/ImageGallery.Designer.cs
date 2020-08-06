c6113b2cfc3eaf6ed9562466b43ffec52ed40c9a
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-08-06 21:31:05 +0530

    Added Panels Dynamically

--- Image Gallery Test/ImageGallery.Designer.cs
+++ Image Gallery Test/ImageGallery.Designer.cs
@@ -1,40 +1,22 @@
-﻿namespace Image_Gallery_Test
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+
+namespace Image_Gallery_Test
 {
-    partial class Form1
+    partial class Form1 
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.SplitContainer splitContainer1;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
-            this.SuspendLayout();
-            // 
-            // splitContainer1
-            // 
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
             this.splitContainer1.IsSplitterFixed = true;
@@ -42,16 +24,12 @@
             this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-            // 
-            // splitContainer1.Panel1
-            // 
+
             this.splitContainer1.Panel1.Margin = new System.Windows.Forms.Padding(2);
             this.splitContainer1.Size = new System.Drawing.Size(764, 749);
             this.splitContainer1.SplitterDistance = 40;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // Form1
-            // 
+
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(764, 749);
@@ -67,11 +45,11 @@
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
+
         }
 
-        #endregion
+     
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
 
