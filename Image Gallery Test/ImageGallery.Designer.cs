c580a61da7865d8be0a2dfa1e46f520c0291593d
23Vatsal <vatsalbhatnagar007@gmail.com>
2020-08-06 21:34:50 +0530

    Variable changes dynamically

--- Image Gallery Test/ImageGallery.Designer.cs
+++ Image Gallery Test/ImageGallery.Designer.cs
@@ -9,31 +9,31 @@ namespace Image_Gallery_Test
 {
     partial class Form1 
     {
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer sc1;
 
         private void InitializeComponent()
         {
-            splitContainer1 = new System.Windows.Forms.SplitContainer();
-            splitContainer1.SuspendLayout();
+            sc1 = new System.Windows.Forms.SplitContainer();
+            sc1.SuspendLayout();
             SuspendLayout();
 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            this.sc1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sc1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.sc1.IsSplitterFixed = true;
+            this.sc1.Location = new System.Drawing.Point(0, 0);
+            this.sc1.Margin = new System.Windows.Forms.Padding(2);
+            this.sc1.Name = "sc1";
+            this.sc1.Orientation = System.Windows.Forms.Orientation.Horizontal;
 
-            this.splitContainer1.Panel1.Margin = new System.Windows.Forms.Padding(2);
-            this.splitContainer1.Size = new System.Drawing.Size(764, 749);
-            this.splitContainer1.SplitterDistance = 40;
-            this.splitContainer1.TabIndex = 0;
+            this.sc1.Panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.sc1.Size = new System.Drawing.Size(764, 749);
+            this.sc1.SplitterDistance = 40;
+            this.sc1.TabIndex = 0;
 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(764, 749);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.sc1);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(810, 810);
             this.Name = "Form1";
@@ -41,8 +41,8 @@ namespace Image_Gallery_Test
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Image Gallery";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.sc1)).EndInit();
+            this.sc1.ResumeLayout(false);
             this.ResumeLayout(false);
 
 
